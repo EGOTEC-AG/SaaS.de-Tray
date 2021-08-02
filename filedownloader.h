377dc9aa164b3025e215eefb3acc73aab9bdc036
commanderk33n <torsten.m85@gmail.com>
2021-08-02 14:46:10 +0200

    filedownloader

---
+++ filedownloader.h
@@ -0,0 +1,32 @@
+#ifndef FILEDOWNLOADER_H
+#define FILEDOWNLOADER_H
+
+#include <QObject>
+#include <QByteArray>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+
+class FileDownloader : public QObject
+{
+    Q_OBJECT
+public:
+    explicit FileDownloader(QUrl fileUrl, QObject *parent = 0);
+
+    virtual ~FileDownloader();
+
+    QByteArray downloadedData() const;
+
+signals:
+    void downloaded();
+    void connectionError();
+
+private slots:
+    void fileDownloaded(QNetworkReply* pReply);
+
+private:
+    QNetworkAccessManager m_WebCtrl;
+    QByteArray m_DownloadedData;
+};
+
+#endif // FILEDOWNLOADER_H
